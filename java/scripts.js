e722f9addee12a5989e687947074cfd1c066c2f7
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-03-06 15:57:15 -0700

    1.5

--- java/scripts.js
+++ java/scripts.js
@@ -55,3 +55,74 @@ for(var i = 0; i < myArrayofDataTypes.length; i++){
 }
 
 document.body.appendChild ( myDataList );
+
+var myHeadingText = 'Our Data-Type Test:';
+// thi is adding HTML to the end of our body.
+
+document.body.innerHTML += `<!--back ticks areused in JS for multi-line string. -->  
+    <h2>
+    `+myHeadingText /*concatenate strings*/+`
+    </h2>`;
+    
+// collecting elements
+
+// get by ID:
+var myHeading = document.getElementById('my-heading');
+myHeading.title = 'This is a heading.' ; // tooltip attribute.
+// myHeading.style.color = 'white'; //Please avoid using inline styles.. this is the css file's job
+myHeading.className = "gray-text";  //use classes instead!
+
+// Events!
+
+// mouse over heading!
+myHeading.addEventListener( 'mouseover', function(event){
+    this.className = ' salmon-text';
+} ) ;
+
+// mouse leaving the heading!
+myHeading.addEventListener( 'mouseleave', function(event){
+    this.className = ' yellowgreen-text';
+} ) ;
+
+// when you stop hovering...
+myHeading.addEventListener( 'click', function(event){
+    // the class we want to add.
+
+    var bigClass = 'big-text'; // check if it already has it!
+    
+    if(!this.className.includes( bigClass ) ) // Good to do checks like this so we don't have 500 of the same class on here if the user keeps clicking!
+
+    { // Exlcamation means not. So i fires on the OPPOSITE.
+    this.className += ' big-text'; // Concaenating a new class!
+    // Remember, you must have a space between classses when assigining multiple.
+    }
+} ) ;
+
+// Let's make a function
+
+// Function keyword, followed by the function name (and parameters if any are needed.)
+
+function addTwoNumbers ( x , y )
+{
+    x = Number ( x );  // Enforce data-type (number.)
+    y = Number ( y ); // Enforce data-type (number.)
+    return ( x + y ); // add the two together.
+};
+
+// Return current date as a string (2020.03.06)
+function currentDayString()
+{   
+    // new date object
+    var date = new Date();
+    // Declare a string we can store date infor in.
+    var dateString= '';
+
+    dateString +=date.getFullYear();     // Year
+    dateString +='/';               // periode for formatting
+    dateString +=( date.getMonth() + 1);    // Month
+    dateString +='/';               // periode for formatting
+    dateString +=( date.getDay()  + 1);  // Date
+    return dateString;
+}
+
+console.log( 'The current date is: \r\n ' + currentDayString());
