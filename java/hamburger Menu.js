c91812c39006bf40279dd0e615cfa00f5e1408fe
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-03-09 11:37:22 -0600

    2.1

--- java/hamburger Menu.js
+++ java/hamburger Menu.js
@@ -9,3 +9,22 @@ var myNavButton = document.querySelector('.menu-button');
 myNavButton.addEventListener('click' , function ( event) {
     myNav.classList.toggle( 'nav-open' );
 });
+
+// let's dig into objects
+
+// JavaScript Object Notation (JSON)
+
+var myObject = { // Valid JSON format
+    name: 'Jerry',
+    age: 61,
+    hobbies:[
+        'snoboarding',
+        'action movies'
+    ]
+};
+
+console.log( myObject );
+
+console.log('Object "name": ' + myObject.name);
+console.log('Object "age": ' + myObject.age);
+console.log('Object "hobbies": ' + myObject.hobbies[0] + ' ' + myObject.hobbies[1]);
