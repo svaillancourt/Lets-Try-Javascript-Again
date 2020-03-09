604df28ee0430425abe4113213634162d94ce81b
Stephane Vaillancourt <svaillancourt@gmail.com>
2020-03-09 15:53:30 -0600

    2.5

--- java/onload.js
+++ java/onload.js
@@ -1,5 +1,9 @@
-alert ("Welcome to my webiste... isn't this annoying >.<");
+window.addEventListener('load', function (event ) { // you need this s
+    
+    alert ("Welcome to my webiste... isn't this annoying >.<");
 
-var myHeading = document.querySelector( 'h1' );
-myHeading.className = 'salmon-text';
-console.log(myHeading);
+    var myHeading = document.querySelector( 'h1' );
+    myHeading.className = 'salmon-text';
+    console.log(myHeading);
+});
+
